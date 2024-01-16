add64a5c6516c2b9b355d6ba41e419f7a5de1094
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-16 18:59:26 -0300

    add myio log to print proc pid

--- experiments/resource_mgmt.ex
+++ experiments/resource_mgmt.ex
@@ -1,3 +1,10 @@
+defmodule MyIO do
+  @spec log(String.t(), String.Chars.t()) :: :ok
+  def log(name, msg) do
+    IO.puts("[#{name}#{inspect(self())}] #{msg}")
+  end
+end
+
 defmodule MyFile.Descriptor do
   defstruct [:path, :pid]
 
@@ -49,24 +56,24 @@ defmodule MyFile.Descriptor do
   defp loop(parent, path) do
     receive do
       {:io_op_req, :open} ->
-        IO.puts("[file] opened [#{path}]")
+        MyIO.log("file", "opened [#{path}]")
         Process.flag(:trap_exit, true)
         send(parent, {:io_op_resp, :open})
         loop(parent, path)
 
       {:io_op_req, :read} ->
-        IO.puts("[file] reading from [#{path}]")
+        MyIO.log("file", "reading from [#{path}]")
         # Fake data. The file system isn't important to this example.
         send(parent, {:io_op_resp, :read, "some data..."})
         loop(parent, path)
 
       {:io_op_req, :close} ->
         # Notice that this case doesn't call `loop/2`.
-        IO.puts("[file] closing file [#{path}]")
+        MyIO.log("file", "closing file [#{path}]")
         send(parent, {:io_op_resp, :close})
 
       {:EXIT, origin, reason} ->
-        IO.puts("[file] got exit from #{inspect(origin)} of reason #{inspect(reason)}")
+        MyIO.log("file", "got exit from #{inspect(origin)} of reason #{inspect(reason)}")
         send(self(), {:io_op_req, :close})
         loop(parent, path)
 
@@ -89,16 +96,14 @@ defmodule MyFile do
   def close(fd), do: Fd.close(fd)
 end
 
-IO.puts("[main] will open file")
-
 {:ok, fd} = MyFile.open("/etc/passwd")
-IO.puts("[main] got fd #{inspect(fd)}")
+MyIO.log("main", "got fd #{inspect(fd)}")
 
 {:ok, data} = MyFile.read(fd)
-IO.puts("[main] got file contents: #{inspect(data)}")
+MyIO.log("main", "got file contents: #{inspect(data)}")
 
 :ok = MyFile.close(fd)
-IO.puts("[main] closed file")
+MyIO.log("main", "closed file")
 
 # Ensure the file process has indeed finished.
 false = Process.alive?(fd.pid)
@@ -110,38 +115,37 @@ String.duplicate("-", 100) |> IO.puts()
 # it will be able to properly clean its resources up.
 
 spawn(fn ->
-  IO.puts("[proc] started #{inspect(self())}")
+  MyIO.log("proc", "started")
   {:ok, fd} = MyFile.open("/etc/hosts")
-  IO.puts("[main] got fd #{inspect(fd)}")
+  MyIO.log("proc", "got fd #{inspect(fd)}")
   true = Process.alive?(fd.pid)
 
-  IO.puts("[proc] will exit abruptly...")
+  MyIO.log("proc", "will exit abruptly...")
   # The file wasn't explicitly closed...
   exit(:whoops)
 end)
 
 Process.sleep(1000)
 String.duplicate("-", 100) |> IO.puts()
-IO.puts("[main] done")
+MyIO.log("main", "done")
 
 # Expected output
 # ===============
 
 # $ mix run experiments/resource_mgmt.ex
 #
-# [main] will open file
-# [file] opened [/etc/passwd]
-# [main] got fd %MyFile.Descriptor{path: "/etc/passwd", pid: #PID<0.122.0>}
-# [file] reading from [/etc/passwd]
-# [main] got file contents: "some data..."
-# [file] closing file [/etc/passwd]
-# [main] closed file
+# [file#PID<0.125.0>] opened [/etc/passwd]
+# [main#PID<0.98.0>] got fd %MyFile.Descriptor{path: "/etc/passwd", pid: #PID<0.125.0>}
+# [file#PID<0.125.0>] reading from [/etc/passwd]
+# [main#PID<0.98.0>] got file contents: "some data..."
+# [file#PID<0.125.0>] closing file [/etc/passwd]
+# [main#PID<0.98.0>] closed file
 # --------------------------------------------------------------------------------------------------
-# [proc] started #PID<0.123.0>
-# [file] opened [/etc/hosts]
-# [main] got fd %MyFile.Descriptor{path: "/etc/hosts", pid: #PID<0.124.0>}
-# [proc] will exit abruptly...
-# [file] got exit from #PID<0.123.0> of reason :whoops
-# [file] closing file [/etc/hosts]
+# [proc#PID<0.126.0>] started
+# [file#PID<0.127.0>] opened [/etc/hosts]
+# [proc#PID<0.126.0>] got fd %MyFile.Descriptor{path: "/etc/hosts", pid: #PID<0.127.0>}
+# [proc#PID<0.126.0>] will exit abruptly...
+# [file#PID<0.127.0>] got exit from #PID<0.126.0> of reason :whoops
+# [file#PID<0.127.0>] closing file [/etc/hosts]
 # --------------------------------------------------------------------------------------------------
-# [main] done
+# [main#PID<0.98.0>] done
