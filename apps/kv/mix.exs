171598f95ad8f8ac4c423306e368a31c5d2308b4
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-19 17:21:14 -0300

    kv: Add application start callback to initialize registry supervisor

--- apps/kv/mix.exs
+++ apps/kv/mix.exs
@@ -18,6 +18,7 @@ defmodule KV.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {KV, []},
       extra_applications: [:logger]
     ]
   end
