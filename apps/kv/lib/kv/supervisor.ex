7519a2e1d095394047aa94f330e6865e258d81d5
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-19 18:44:40 -0300

    kv: Fix ordering and strategy issues in kv.supervisor

--- apps/kv/lib/kv/supervisor.ex
+++ apps/kv/lib/kv/supervisor.ex
@@ -9,10 +9,16 @@ defmodule KV.Supervisor do
   @impl Supervisor
   def init([]) do
     children = [
-      {KV.Registry, name: KV.Registry},
-      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
+      # `KV.BucketSupervisor` must be started *before* `KV.Registry`, as the
+      # registry invokes the bucket supervisor when creating new buckets.
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
+      {KV.Registry, name: KV.Registry}
     ]
 
-    Supervisor.init(children, strategy: :one_for_one)
+    # If `KV.Registry` crashes, `KV.BucketSupervisor` and all its children (all
+    # the buckets) must terminate too. Otherwise, with the new registry, each
+    # of the old buckets would be unreachable. I.e., `:one_for_one` would cause
+    # a leak.
+    Supervisor.init(children, strategy: :one_for_all)
   end
 end
