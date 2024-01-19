4e719c7d2280165acf58fa3a8212bd2d33030cac
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-19 10:32:31 -0300

    kv: Add delete function

--- apps/kv/lib/kv/bucket.ex
+++ apps/kv/lib/kv/bucket.ex
@@ -30,4 +30,14 @@ defmodule KV.Bucket do
   def put(bucket, key, val) do
     Agent.update(bucket, &Map.put(&1, key, val))
   end
+
+  @doc """
+  Deletes the value of the given key from the bucket.
+
+  Returns the current value if it exists; `nil` otherwise.
+  """
+  @spec delete(t(), k()) :: v() | nil
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
 end
--- apps/kv/test/kv/bucket_test.exs
+++ apps/kv/test/kv/bucket_test.exs
@@ -12,4 +12,14 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "key:1", "value 1")
     assert KV.Bucket.get(bucket, "key:1") == "value 1"
   end
+
+  test "deletes the key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "key:1", "value 1")
+    assert KV.Bucket.get(bucket, "key:1") == "value 1"
+
+    assert KV.Bucket.delete(bucket, "key:1") == "value 1"
+    assert KV.Bucket.get(bucket, "key:1") == nil
+
+    assert KV.Bucket.delete(bucket, "key:1") == nil
+  end
 end
