e17db113fa5b9e9eb4828f2b3980ede36b303b05
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-19 10:24:56 -0300

    kv: Use test setup

--- apps/kv/test/kv/bucket_test.exs
+++ apps/kv/test/kv/bucket_test.exs
@@ -1,11 +1,15 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
-    {:ok, b} = KV.Bucket.start_link()
-    assert KV.Bucket.get(b, "key:1") == nil
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link()
+    %{bucket: bucket}
+  end
 
-    KV.Bucket.put(b, "key:1", "value 1")
-    assert KV.Bucket.get(b, "key:1") == "value 1"
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "key:1") == nil
+
+    KV.Bucket.put(bucket, "key:1", "value 1")
+    assert KV.Bucket.get(bucket, "key:1") == "value 1"
   end
 end
