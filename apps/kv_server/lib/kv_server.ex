88f3288b09dd7389888e34285923295241568e6b
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-25 18:06:01 -0300

    kv_server: add initial server impl

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,42 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for `KVServer`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} =
+      :gen_tcp.listen(port, [
+        :binary,
+        packet: :line,
+        active: false,
+        reuseaddr: true
+      ])
 
-  ## Examples
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
 
-      iex> KVServer.hello()
-      :world
+  # this acceptor is fragile and if it errors out, currently, the application
+  # won't be able to serve any more requests
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
 
-  """
-  def hello do
-    :world
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    # this will error out when the client closes the connection
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
