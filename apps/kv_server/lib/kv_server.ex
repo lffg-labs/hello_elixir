4296001a57d5c3f4fcb375dd79d11d98446cea00
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-25 18:26:17 -0300

    kv_server: handle failures and serve clients on separate processes

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -2,7 +2,7 @@ defmodule KVServer do
   require Logger
 
   def accept(port) do
-    {:ok, socket} =
+    {:ok, listen_socket} =
       :gen_tcp.listen(port, [
         :binary,
         packet: :line,
@@ -11,23 +11,37 @@ defmodule KVServer do
       ])
 
     Logger.info("Accepting connections on port #{port}")
-    loop_acceptor(socket)
+    loop_acceptor(listen_socket)
   end
 
   # this acceptor is fragile and if it errors out, currently, the application
   # won't be able to serve any more requests
-  defp loop_acceptor(socket) do
-    {:ok, client} = :gen_tcp.accept(socket)
-    serve(client)
-    loop_acceptor(socket)
+  defp loop_acceptor(listen_socket) do
+    {:ok, client_socket} = :gen_tcp.accept(listen_socket)
+
+    {:ok, pid} =
+      Task.Supervisor.start_child(KVServer.TaskSupervisor, fn ->
+        serve(client_socket)
+      end)
+
+    :ok = :gen_tcp.controlling_process(client_socket, pid)
+
+    loop_acceptor(listen_socket)
   end
 
   defp serve(socket) do
+    {:ok, addr} = :inet.peername(socket)
+    Logger.info("serving #{inspect(addr)}")
+
+    answer_with_lines(socket)
+  end
+
+  defp answer_with_lines(socket) do
     socket
     |> read_line()
     |> write_line(socket)
 
-    serve(socket)
+    answer_with_lines(socket)
   end
 
   defp read_line(socket) do
--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -1,19 +1,27 @@
 defmodule KVServer.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   @impl true
   def start(_type, _args) do
+    port = System.get_env("PORT", "4040") |> String.to_integer()
+
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      # By default, the `Task` module's `child_spec/1` hard code the `restart`
+      # option as `temporary`. In the server acceptor process, however, it's
+      # necessary to restart it in the case of failures.
+      #
+      # While one could also implement the server's acceptor as a GenServer,
+      # we could also use a task and override it's `restart` option by using
+      # the `Supervisor.child_spec/2` function.
+      Supervisor.child_spec(
+        {Task, fn -> KVServer.accept(port) end},
+        restart: :permanent
+      )
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
